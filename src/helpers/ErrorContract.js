03ebf65759777703a5b122fd94aeef7217498375
Bram Adl <thenamesbram@gmail.com>
2022-05-15 13:48:55 +0700

    docs: document internal error to error contract

--- src/helpers/ErrorContract.js
+++ src/helpers/ErrorContract.js
@@ -4,6 +4,8 @@ class ErrorContract {
   NO_API_FILES = 'No API files found under the API folder directory. Start by creating a folder for your endpoint inside the API folder.';
 
   INVALID_BEARER_TOKEN = 'Please provide a valid bearer token to access this resource';
+
+  INTERNAL_ERROR = 'An error has occured. Please try again later.';
 }
 
 module.exports = new ErrorContract();
