1da14fcc16aab9bc42552c03e9937dba96b0b70e
Bram Adl <thenamesbram@gmail.com>
2022-05-15 13:51:18 +0700

    fix: snake case to camel case and validation rule

--- src/api/Auth/AuthValidator.js
+++ src/api/Auth/AuthValidator.js
@@ -9,8 +9,8 @@ const login = {
 
 const register = {
   body: {
-    first_name: Joi.string().max(20).allow(null),
-    last_name: Joi.string().max(30).required(),
+    firstName: Joi.string().max(20).allow(null, '').optional(),
+    lastName: Joi.string().max(30).required(),
     email: Joi.string().email().required(),
     password: Joi.string().min(6).required(),
   },
