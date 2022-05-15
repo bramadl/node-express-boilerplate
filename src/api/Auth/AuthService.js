6df16536db848d357658d8d76cdae0a724ee8658
Bram Adl <thenamesbram@gmail.com>
2022-05-15 13:52:15 +0700

    feat: auth and user service for crud management and authentication

--- src/api/Auth/AuthService.js
+++ src/api/Auth/AuthService.js
@@ -1,18 +1,29 @@
+const { PrismaClient } = require('@prisma/client');
 const bcrypt = require('bcrypt');
+const httpStatus = require('http-status');
 const jwt = require('jsonwebtoken');
 const moment = require('moment');
 
+const UserRepository = require('../Users/UserRepository');
 const {
   jwt: { expiry, secret },
 } = require('../../config/env');
+const ApiError = require('../../helpers/ApiError');
+
+const prisma = new PrismaClient();
 
 class AuthService {
+  /**
+   * Generate an access token with the given user.
+   *
+   * @param {User} user - The user model
+   * @returns {Token}
+   */
   generateAccessToken(user) {
     const {
       id,
-      role: { name: roleName },
+      role: { name: role },
     } = user;
-
     const exp = moment().add(expiry, 'minutes').unix();
 
     const token = jwt.sign(
@@ -20,7 +31,7 @@ class AuthService {
         sub: id,
         iat: moment().unix(),
         exp,
-        role: roleName,
+        role,
       },
       secret,
     );
@@ -39,17 +50,27 @@ class AuthService {
    * @returns {Promise<User>}
    */
   async signInWithEmailPassword(email, password) {
-    return {
-      id: 0,
-      first_name: 'Bram',
-      last_name: 'Adl',
-      email,
-      password: await bcrypt.hash(password, 10),
-      role: {
-        id: 0,
-        name: 'Owner',
-      },
-    };
+    const user = await prisma.user.findUnique({
+      where: { email },
+    });
+
+    if (!user) {
+      throw new ApiError(
+        httpStatus.NOT_FOUND,
+        'No user with the given email is found.',
+      );
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordMatch) {
+      throw new ApiError(httpStatus.FORBIDDEN, 'Credentials do not match.');
+    }
+
+    return prisma.user.findUnique({
+      where: { email },
+      select: UserRepository.user,
+    });
   }
 
   /**
@@ -62,17 +83,22 @@ class AuthService {
    * @returns {Promise<User>}
    */
   async signUpWithEmailPassword(firstName, lastName, email, password) {
-    return {
-      id: 0,
-      first_name: firstName,
-      last_name: lastName,
-      email,
-      password: await bcrypt.hash(password, 10),
-      role: {
-        id: 0,
-        name: 'Owner',
+    if (await prisma.user.findUnique({ where: { email } })) {
+      throw new ApiError(httpStatus.CONFLICT, 'Email has been taken.');
+    }
+
+    const user = await prisma.user.create({
+      data: {
+        firstName,
+        lastName,
+        email,
+        password: await bcrypt.hash(password, 10),
+        roleId: 2, // DEFAULTS TO ADMIN
       },
-    };
+      select: UserRepository.user,
+    });
+
+    return user;
   }
 }
 
--- src/api/Users/UserService.js
+++ src/api/Users/UserService.js
@@ -1,17 +1,47 @@
-/* eslint-disable no-console */
+const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcrypt');
+const httpStatus = require('http-status');
+
+const UserRepository = require('./UserRepository');
+const ApiError = require('../../helpers/ApiError');
+const PaginationBuilder = require('../../helpers/PaginationBuilder');
+const StrHelper = require('../../helpers/StrHelper');
+
+const prisma = new PrismaClient();
+
 class UserService {
   /**
    * Retrieve all the users data from database
    * with pagination strategy.
    *
    * @param {Express.Request.Query} query - The request query object
-   * @param {String | undefined} query.per_page - The per page value as string
    * @param {String | undefined} query.current_page - The current page value as string
+   * @param {String | undefined} query.per_page - The per page value as string
    * @param {String | undefined} query.search - The search value as string
    * @return {Promise<User[]>}
    */
   async getAllUsers(query) {
-    console.log({ query });
+    const { search, perPage, currentPage } = PaginationBuilder.sanitizeQuery(query);
+
+    const [total, data] = await prisma.$transaction([
+      prisma.user.count(),
+      prisma.user.findMany({
+        ...(search !== '' && { where: UserRepository.filter(search) }),
+        select: UserRepository.user,
+        take: perPage,
+        skip: perPage * (currentPage - 1),
+        orderBy: { createdAt: 'asc' },
+      }),
+    ]);
+
+    return {
+      users: data,
+      pagination: PaginationBuilder.buildPagination(
+        currentPage,
+        perPage,
+        total,
+      ),
+    };
   }
 
   /**
@@ -22,7 +52,22 @@ class UserService {
    * @return {Promise<User>}
    */
   async getUserById(userId) {
-    console.log({ userId });
+    const user = await prisma.user.findUnique({
+      where: {
+        id: userId,
+      },
+      select: UserRepository.user,
+    });
+
+    // No user with the given ID is found.
+    if (!user) {
+      throw new ApiError(
+        httpStatus.NOT_FOUND,
+        'The user with the given ID could not be found.',
+      );
+    }
+
+    return user;
   }
 
   /**
@@ -30,13 +75,34 @@ class UserService {
    * with the given request body.
    *
    * @param {Object} userBody - The request body payload
-   * @param {String} userBody.name - The user name
+   * @param {String} userBody.firstName - The user first name
+   * @param {String} userBody.lastName - The user last name
    * @param {String} userBody.email - The user email
-   * @param {String | undefined} userBody.phone - The user phone
+   * @param {String | undefined} userBody.phoneNumber - The user phone
    * @return {Promise<User>}
    */
   async createUser(userBody) {
-    console.log({ userBody });
+    // User with the given email has been registered.
+    if (await prisma.user.findUnique({ where: { email: userBody.email } })) {
+      throw new ApiError(
+        httpStatus.CONFLICT,
+        'A user with the given email has been registered.',
+      );
+    }
+
+    const user = await prisma.user.create({
+      data: {
+        firstName: userBody.firstName,
+        lastName: userBody.lastName,
+        email: userBody.email,
+        phoneNumber: userBody.phoneNumber,
+        password: await bcrypt.hash(StrHelper.generateRandomString(), 10),
+        roleId: 3, // DEFAULTS TO EDITOR
+      },
+      select: UserRepository.user,
+    });
+
+    return user;
   }
 
   /**
@@ -45,13 +111,48 @@ class UserService {
    *
    * @param {String} userId - The user id
    * @param {Object} userBody - The request body payload
-   * @param {String} userBody.name - The user name
+   * @param {String} userBody.firstName - The user first name
+   * @param {String} userBody.lastName - The user last name
    * @param {String} userBody.email - The user email
-   * @param {String | undefined} userBody.phone - The user phone
+   * @param {String | undefined} userBody.phoneNumber - The user phone
    * @return {Promise<User>}
    */
   async updateUser(userId, userBody) {
-    console.log({ userId, userBody });
+    const user = await prisma.user.findUnique({ where: { id: userId } });
+
+    // No user with the given id is found.
+    if (!user) {
+      throw new ApiError(
+        httpStatus.NOT_FOUND,
+        'The user with the given ID could not be found.',
+      );
+    }
+
+    const otherRegisteredEmail = await prisma.user.findMany({
+      where: {
+        id: {
+          not: user.id,
+        },
+      },
+    });
+
+    // The email that the user tried to change has been taken.
+    if (
+      otherRegisteredEmail.map((_user) => _user.email).includes(userBody.email)
+    ) {
+      throw new ApiError(httpStatus.CONFLICT, 'Email has been taken.');
+    }
+
+    return prisma.user.update({
+      where: { id: userId },
+      data: {
+        firstName: userBody.firstName,
+        lastName: userBody.lastName,
+        email: userBody.email,
+        phoneNumber: StrHelper.nullifyEmptyString(userBody.phoneNumber),
+      },
+      select: UserRepository.user,
+    });
   }
 
   /**
@@ -59,10 +160,17 @@ class UserService {
    * with the given id.
    *
    * @param {String} userId - The user id
-   * @return {Promise<User>}
    */
   async deleteUser(userId) {
-    console.log({ userId });
+    // No user with the given id is found.
+    if (!(await prisma.user.findUnique({ where: { id: userId } }))) {
+      throw new ApiError(
+        httpStatus.NOT_FOUND,
+        'The user with the given ID could not be found.',
+      );
+    }
+
+    await prisma.user.delete({ where: { id: userId } });
   }
 }
 
